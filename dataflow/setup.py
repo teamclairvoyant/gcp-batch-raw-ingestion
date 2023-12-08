b527c38de571dff41c8ace0128c21b7f13ff0081
amakadam1994 <amar.kadam@clairvoyantsoft.com>
2023-12-08 09:26:51 +0530

    install_requires changed

--- dataflow/setup.py
+++ dataflow/setup.py
@@ -1,9 +1,4 @@
 import setuptools
-#
-#
-# with open('requirements.txt') as f:
-#     print("File opened")
-#     required = f.read().splitlines()
 
 setuptools.setup(
     name='Dataflow-Ingestion',
@@ -17,13 +12,12 @@ setuptools.setup(
         ,"pymongo==3.9.0"
         ,"pymongo[srv]"
         ,"mysql-connector-python==8.0.23"
-        ,"pycryptodome==3.17"
         ,"google-cloud-secret-manager==2.16.1"
         ,"protobuf==3.20.3"
+        ,"pycryptodome==3.17"
         ,"numpy==1.24.3"
         ,"grpcio-status==1.48.2"
+        ],
 
-                       ],
-    # install_requires=required,
     packages=setuptools.find_packages()
 )
